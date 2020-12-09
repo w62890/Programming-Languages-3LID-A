cc76f4ed14cc5626835813f028cd8964ae64ecbc
Asanali Bulatov <w62890@student.wsiz.edu.pl>
2020-12-09 22:57:35 +0600

    Casino in progress

---
+++ TDD_Dice/Casino.Tests/DiceTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Casino.Tests
+{
+    public class DiceTests
+    {
+        [Fact]
+        public void Dice_is_rollable()
+        {
+            var dice = new Dice();
+
+            dice.Roll();
+
+            dice.First.Should().BeInRange(1, 6);
+            dice.Second.Should().BeInRange(1, 6);
+        }
+
+        [Fact]
+        public void Dice_to_string()
+        {
+            var dice = new Dice();
+
+            dice.ToString().Should().Be($"{dice.First}-{dice.Second}");
+        }
+    }
+}
---
+++ TDD_Dice/Casino/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Casino
+{
+    public class Dice
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        public Dice()
+        {
+            
+        }
+
+        public void Roll()
+        {
+            var rand = new Random();
+            First = rand.Next() % 5 + 1;
+            Second = rand.Next() % 5 + 1;
+        }
+
+        public override string ToString() => $"{First}-{Second}";
+    }
+}
